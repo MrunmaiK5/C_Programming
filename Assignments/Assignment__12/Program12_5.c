0b7efccb97f24ca67b3183f1e9a251a7ea50b27d
MrunmaiK5 <mrunmai0205@gmail.com>
2025-10-24 18:42:55 +0530

    Added Assignment__12

--- Assignments/Assignment__12/Program12_5.c
+++ Assignments/Assignment__12/Program12_5.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  is_divisible_by_five
+//  Description:    Checks wether the given number is divisible by 5.
+//  Input:          Integer
+//  Output:         Boolean
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           24/10/20025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 bool is_divisible_by_five(int number)
 {
     bool bAns=false;
