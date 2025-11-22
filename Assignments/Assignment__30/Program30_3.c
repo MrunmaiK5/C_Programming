dcadd00a021e65f1986c186ac5c12630a75c37f8
MrunmaiK5 <mrunmai0205@gmail.com>
2025-11-22 12:02:14 +0530

    Added Assignment__30

---
+++ Assignments/Assignment__30/Program30_3.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  Pattern
+//  Description:    Displays a pattern.
+//  Input:          Integer
+//  Output:         Void
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           22/11/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i=0,j=0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input\n");
+        printf("Rows and columns should be same\n");
+        return;
+    }
+
+    for(i=1; i<=iRow; i++)
+    {
+        for(j=1; j<=iCol; j++)
+        {
+            if (i==j)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+               
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1=0, iValue2=0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
